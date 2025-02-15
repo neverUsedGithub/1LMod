6b7b74e70f0d53246ec125f815439d19edfe341f
neverUsedGithub <81071335+neverUsedGithub@users.noreply.github.com>
2025-02-15 20:18:20 +0100

    add on diamondfire check

--- src/main/java/onelone/onelmod/mixins/ClientCommonNetworkHandlerMixin.java
+++ src/main/java/onelone/onelmod/mixins/ClientCommonNetworkHandlerMixin.java
@@ -1,10 +1,10 @@
 package onelone.onelmod.mixins;
 
 import net.minecraft.client.network.ClientCommonNetworkHandler;
-import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.network.packet.Packet;
 import net.minecraft.text.Text;
+import onelone.onelmod.client.OneLClient;
 import onelone.onelmod.client.config.Config;
 import onelone.onelmod.client.toast.OneLToast;
 import org.spongepowered.asm.mixin.Mixin;
@@ -16,7 +16,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ClientCommonNetworkHandlerMixin {
     @Inject(method = "onPacketException", at = @At("HEAD"), cancellable = true)
     public void onPacketException(Packet<?> packet, Exception exception, CallbackInfo ci) {
-        if (Config.INSTANCE.getNoNetworkProtocol()) {
+        if (OneLClient.INSTANCE.onDiamondFire() && Config.INSTANCE.getNoNetworkProtocol()) {
             OneLToast.Companion.show(Items.BARRIER, Text.of("Network Protocol Error"), Text.translatable("onelmod.toast.network_protocol_error"));
             ci.cancel();
         }
