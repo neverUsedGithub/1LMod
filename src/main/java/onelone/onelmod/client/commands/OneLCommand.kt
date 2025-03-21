098cb283aa47ad55c5202b5879abd5100c0c7dbd
neverUsedGithub <81071335+neverUsedGithub@users.noreply.github.com>
2025-03-21 14:08:26 +0100

    add smallcaps command

--- src/main/java/onelone/onelmod/client/commands/OneLCommand.kt
+++ src/main/java/onelone/onelmod/client/commands/OneLCommand.kt
@@ -8,6 +8,9 @@ import com.mojang.brigadier.exceptions.CommandSyntaxException
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager
 import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource
 import net.minecraft.client.MinecraftClient
+import net.minecraft.text.ClickEvent
+import net.minecraft.text.HoverEvent
+import net.minecraft.text.Style
 import net.minecraft.text.Text
 import net.minecraft.util.Formatting
 import onelone.onelmod.client.OneLClient
@@ -16,25 +19,59 @@ import onelone.onelmod.client.language.Compiler
 import onelone.onelmod.client.language.LanguageError
 
 object OneLCommand {
+    private const val NORMAL_CHARACTERS = "abcdefghijklmnopqrstuvwxyz"
+    private const val SMALLCAPS_CHARACTERS = "ᴀʙᴄᴅᴇꜰɢʜɪᴊᴋʟᴍɴᴏᴘǫʀsᴛᴜᴠᴡxʏᴢ"
+
     fun register(dispatcher: CommandDispatcher<FabricClientCommandSource?>) {
         val expressionCommand = ClientCommandManager.literal("expr")
             .then(ClientCommandManager.argument("expression", StringArgumentType.greedyString())
                 .executes(executeExpression)
             )
 
+        val smallcapsCommand = ClientCommandManager.literal("smallcaps")
+            .then(ClientCommandManager.argument("text", StringArgumentType.greedyString())
+                .executes(smallcapsCallback)
+            )
+
         val command = dispatcher.register(ClientCommandManager.literal("onelmod")
             .then(ClientCommandManager.literal("nbt").executes(showNBT))
             .then(expressionCommand)
             .then(ClientCommandManager.literal("gui").executes(openSettings))
+            .then(smallcapsCommand)
             .executes(openSettings)
         )
 
+        dispatcher.register(smallcapsCommand)
         dispatcher.register(expressionCommand)
         dispatcher.register(
             ClientCommandManager.literal("onel")
                 .redirect(command)
                 .executes(openSettings)
         )
+    }
+
+    private val smallcapsCallback = { ctx: CommandContext<FabricClientCommandSource> ->
+        val text = StringArgumentType.getString(ctx, "text")
+        var mapped = ""
+
+        for (char in text) {
+            val index = NORMAL_CHARACTERS.indexOf(char)
+
+            mapped += if (index != -1) {
+                SMALLCAPS_CHARACTERS[index]
+            } else {
+                char
+            }
+        }
+
+        OneLClient.logChat(
+            Text.of(mapped)
+                .getWithStyle(Style.EMPTY
+                    .withHoverEvent(HoverEvent(HoverEvent.Action.SHOW_TEXT, Text.translatable("onelmod.command.smallcaps.copy")))
+                    .withClickEvent(ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, mapped)))[0]
+        )
+
+        Command.SINGLE_SUCCESS
     }
 
     private val openSettings = { _: CommandContext<FabricClientCommandSource> ->
